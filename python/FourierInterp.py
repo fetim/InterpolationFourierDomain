e5a7609975fc28cc27527b64b8ee3f2b798f80d5
Felipe <felipetimoteo@id.uff.br>
2017-07-17 11:44:14 -0300

    1D - Fourier Interpolation update

--- python/FourierInterp.py
+++ python/FourierInterp.py
@@ -1,43 +1,58 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 """
-Created on Sun Jul 16 21:00:16 2017
+FourierInterp:
+
+Interpolation in Fourier Domain.
+The number of samples in Frequency domain
+is proportional to the sampling rate in time 
+domain.
 
 @author: felipe
+@email : felipetimoteo@id.uff.br
 """
 
-import numpy as np
-import matplotlib.pyplot as plt
-import scipy.fftpack
+def main():
+    #%%
+    import numpy as np
+    import matplotlib.pyplot as plt
+    import scipy.fftpack 
+     
+    incr_sampling = 2
+    L  = 2.0
+    dt = 0.001
+    Nt = L/dt + 1
+    
+     
+    t = np.linspace(0.0,dt*Nt,Nt)
+    y = np.exp(-100*(t-0.3)*(t-0.3))
+     
+    plt.figure(1)
+    plt.plot(t,y,color='k',linestyle='dashed',marker='o')
+    plt.show()
+     
+    
+    Y = scipy.fftpack.rfft(y)
+     
+    size_Y = np.size(Y)    
+    X = np.linspace(0.0,1.0/(2.0*dt),Nt/2)
+     
+    
+    Y_ex = np.insert(Y,size_Y/2,np.zeros(incr_sampling*Nt))
+     
+    plt.figure(2)
+     
+    plt.plot(X,2.0/Nt*np.abs(Y[:Nt/2]))
+    plt.show()
+     
+    y_interp = (incr_sampling+1)*scipy.fftpack.irfft(Y_ex)
+    t_interp = np.linspace(0.0,dt*Nt,(incr_sampling+1)*Nt)
+     
+    plt.figure(1)
+    plt.plot(t,y,color='k',linestyle='dashed',marker='o')
+    plt.plot(t_interp,y_interp,color='r',marker='o')
+    plt.show()
 
-incr_sampling = 20
-dt = 0.01
-Nt = 100
-
-
-t = np.linspace(0.0,dt*Nt,Nt)
-y = np.exp(-100*(t-0.3)*(t-0.3))
-
-plt.figure(1)
-plt.plot(t,y,color='k',linestyle='dashed',marker='o')
-plt.show()
-
-Y = scipy.fftpack.rfft(y)
-
-
-X = np.linspace(0.0,1.0/(2.0*dt),Nt/2)
-
-Y_ex = np.append(Y,np.zeros(incr_sampling*Nt))
-
-plt.figure(2)
-
-plt.plot(X,2.0/Nt*np.abs(Y[:Nt/2]))
-plt.show()
-
-y_interp = (incr_sampling+1)*scipy.fftpack.irfft(Y_ex)
-t_interp = np.linspace(0.0,dt*Nt,(incr_sampling+1)*Nt)
-
-plt.figure(1)
-plt.plot(t,y,color='k',linestyle='dashed',marker='o')
-plt.plot(t_interp,y_interp,color='r',marker='o')
-plt.show()
+    #%%
+if __name__=="__main__":
+    main()
