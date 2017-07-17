9eb0e97dbf58d93fff6cd390b3fd41fafd95ea27
Felipe <felipetimoteo@id.uff.br>
2017-07-17 19:15:06 -0300

    Fortran Fourier transform FFTW

---
+++ fortran/FourierInterp.f90
@@ -0,0 +1,75 @@
+PROGRAM FourierInterpolation
+
+  IMPLICIT NONE
+
+  INCLUDE "fftw3.f90"
+
+  INTEGER    ,PARAMETER                :: sp=4, dp=8, qp=16
+  INTEGER(sp),PARAMETER                :: seed = 8198272  
+  INTEGER                              :: k,Nt,Nt_c
+  REAL(dp)  ,PARAMETER                 :: pi = 4.0*atan(1.0)
+  REAL(dp)                             :: dt,t0,tf
+  REAL(dp)                             :: df,f0,ff,Fs
+  REAL(dp)                             :: frequency
+  REAL(dp)                             :: plan_backward,plan_forward
+  
+  REAL(dp)   ,DIMENSION(:),ALLOCATABLE    :: in
+  REAL(dp)   ,DIMENSION(:),ALLOCATABLE    :: t,y,f
+  COMPLEX(dp),DIMENSION(:),ALLOCATABLE :: out 
+
+  call srand(seed)
+  t0 = 0.0
+  tf = 1.0
+  Nt = 100
+  Nt_c = Nt/2 + 1
+
+  ALLOCATE(in(Nt))
+  ALLOCATE(out(Nt))
+  ALLOCATE(t(Nt))
+  ALLOCATE(y(Nt))
+  ALLOCATE(f(Nt_c))
+
+
+  dt = (tf - t0)/(Nt-1)
+  Fs = 1/dt
+  print*,''
+  print*,'Sample rate = ',dt, 'Frequency sampling = ', Fs
+  print*,'Nyquist Frequency = ', Fs/4
+  print*,''
+
+  t = (/(t0 + (k-1)*dt,k=1,Nt,1)/)
+  
+
+!  y = exp(-1000*(t-0.3)*(t-0.3))
+  frequency = 10 !Hz
+  y = sin(2*pi*frequency*t)
+  
+
+  open(25,file='gaussfunction.dat',status='unknown',form='formatted')
+  do k=1,Nt     
+!     y(k) = rand()
+     write(25,*) t(k),y(k)
+  end do
+  close(25)
+
+  
+  CALL dfftw_plan_dft_r2c_1d_ (plan_forward,Nt,in,out,FFTW_ESTIMATE)
+
+  CALL dfftw_execute_ (plan_forward)
+  
+  f0 = 0.0
+  tf =2.0/(2*dt) 
+  df = (tf - f0)/(Nt_c-1)
+  
+  f  = (/(f0 + (k-1)*df ,k=1,Nt_c,1)/)
+  open(26,file='gaussfunction_fourierdomain.dat',status='unknown',form='formatted')
+
+  do k=1,Nt_c
+     write(26,*) f(k),2.0/Nt*abs(out(k))
+  end do
+
+  close(26)
+
+  call dfftw_destroy_plan_ ( plan_forward )
+
+END PROGRAM FourierInterpolation
---
+++ fortran/plotgraph.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+plotgraph.py:
+
+Plot a Graph from a ascii file with two columns.
+1st column is x direction
+2nd comlun is y direction
+
+@author: felipe
+@email : felipetimoteo@id.uff.br
+"""
+
+def main():
+    
+    plot_from_file('gaussfunction.dat')
+
+    plot_from_file('gaussfunction_fourierdomain.dat')
+
+def plot_from_file(filename):
+    import numpy as np
+    import matplotlib.pyplot as plt
+    
+    #filename="gaussfunction.dat"
+    
+    data = np.loadtxt(filename,float)
+    
+    x = data[:,0]
+    y = data[:,1]
+
+
+    plt.plot(x,y,marker='o')
+    plt.show()
+    
+if __name__=="__main__":
+    main()
